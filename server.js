6a6c74252ff65aa50b42b0dbc298cd96aef1e406
gv66co <gerovejo66@gmail.com>
2025-12-28 13:06:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post("/rotate", (req, res) => {
+app.post("/", (req, res) => {
   const text = req.body.text || "";
   const rotated = text.split("").reverse().join("");
   res.json({ rotated });
 });
 
-// Cloud Functions 2nd gen entrypoint:
 exports.rotateHandler = app;
